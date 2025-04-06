91a160f4cbedbfb76e7ed053123f949ac8876feb
sathish <sathishkannan162@gmail.com>
2025-04-06 15:33:25 +0530

    lint: fix lint issues

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -10,6 +10,7 @@ interface ButtonProps {
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
+    // biome-ignore lint/a11y/useButtonType: <explanation>
     <button
       className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
