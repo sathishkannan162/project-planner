6119cafeddc9394da6cb28901da3ce881943d8ee
Sathish K M <sathish.km@iouring.com>
2025-09-15 15:03:37 +0530

    Fix import rules for ui and app repo

--- apps/project-planner/app/page.tsx
+++ apps/project-planner/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
               href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
               target="_blank"
               rel="noopener noreferrer"
-              className='flex justify-between'
+              className="flex justify-between"
             >
               <Image
                 className={styles.logo}
@@ -57,7 +57,7 @@ export default function Home() {
               <div>Deploy now</div>
             </a>
           </Button>
-          <Button variant='outline'>
+          <Button variant="outline">
             <a
               href="https://turbo.build/repo/docs?utm_source"
               target="_blank"
