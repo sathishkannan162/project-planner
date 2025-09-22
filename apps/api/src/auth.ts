7f0534e2239c23f1bc4b8ad7c7ea93d8f960a576
Sathish K M <sathish.km@iouring.com>
2025-09-22 12:40:25 +0530

    Add better-auth server-side

---
+++ apps/api/src/auth.ts
@@ -0,0 +1,15 @@
+import * as schema from "./db/schema";
+
+import { betterAuth } from "better-auth";
+import { db } from "./db";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
